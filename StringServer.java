be342162a0ca581b23911d617e8efc9467c6a1db
jliu312 <85335934+jliu312@users.noreply.github.com>
2023-01-25 16:30:02 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+class Handler implements URLHandler {
+    String str = "";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                str = str + parameters[1] + "\n";
+            }
+        } 
+        return str;
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
